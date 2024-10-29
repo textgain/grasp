a18c6ed56c685f0a59fc4c4d85e2d23f39ddeb82
tom-de-smedt <tom@organisms.be>
2024-10-29 22:37:36 +0100

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ PATH = os.path.dirname(__file__)
 
 setup(
                   name = 'grasp',
-               version = '3.2.2',
+               version = '3.2.3',
            description = 'Simple NLP toolkit',
                license = 'BSD',
                 author = 'Textgain',
